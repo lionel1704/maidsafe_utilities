1770018c7420f37abda48219cebae0c6e1cf7086
MaidSafe-QA <qa@maidsafe.net>
2016-06-24 10:07:42 +0000

    Updated documentation.

--- master/implementors/core/fmt/trait.Pointer.js
+++ master/implementors/core/fmt/trait.Pointer.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['serde'] = [];implementors['maidsafe_utilities'] = [];
+implementors['libc'] = [];implementors['serde'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/nonzero/trait.Zeroable.js
+++ master/implementors/core/nonzero/trait.Zeroable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['libc'] = [];implementors['serde'] = [];implementors['maidsafe_utilities'] = [];
+implementors['bitflags'] = [];implementors['libc'] = [];implementors['serde'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
