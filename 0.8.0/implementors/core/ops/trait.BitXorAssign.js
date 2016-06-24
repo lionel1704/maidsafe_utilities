95b51f73e2ae55a5c153a1a6e7e5b971a144d4c5
MaidSafe-QA <qa@maidsafe.net>
2016-06-24 14:26:16 +0000

    Updated documentation.

---
+++ 0.8.0/implementors/core/ops/trait.BitXorAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['mio'] = [];implementors['serde'] = [];implementors['maidsafe_utilities'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
