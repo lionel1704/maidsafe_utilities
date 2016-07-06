5181802f54b4d504ec0b446d11fddac40af59c88
MaidSafe-QA <qa@maidsafe.net>
2016-07-06 13:44:55 +0000

    Updated documentation.

--- master/implementors/collections/string/trait.ToString.js
+++ master/implementors/collections/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['mio'] = [];implementors['serde'] = [];implementors['maidsafe_utilities'] = [];
+implementors['antidote'] = [];implementors['humantime'] = [];implementors['regex_syntax'] = [];implementors['serde'] = [];implementors['mio'] = [];implementors['maidsafe_utilities'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
